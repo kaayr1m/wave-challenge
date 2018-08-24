5a14da34e6014368fffbf388134556689238c6b0
Karim Kawambwa <karimkawambwa@icloud.com>
2018-08-24 19:56:26 -0400

    test put (takes long time :( )

--- functions/test/api.js
+++ functions/test/api.js
@@ -8,14 +8,25 @@ chai.use(chaiHttp);
 
 describe('Request', () => {
   describe('/board', () => {
-      it('it should return current board', (done) => {
-        chai.request(server)
-            .get('/')
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.text.should.be.eql('+++++++++');
-              done();
-            });
-      });
+    it('it should return current board', (done) => {
+      chai.request(server)
+          .get('/')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.text.should.be.eql('+++++++++');
+            done();
+          });
+    });
+  });
+  describe('/:board', () => {
+    it('it should return new move by o', (done) => {
+      chai.request(server)
+          .put('/+++++++++')
+          .end((err, res) => {
+            res.should.have.status(200);
+            assert.equal(true, res.text.indexOf('o') !== -1);
+            done();
+          });
+    }).timeout(10000);
   });
 });
