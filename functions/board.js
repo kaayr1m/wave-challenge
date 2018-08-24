22a7a39869ea726e9a892ef3888b2579121f0dc5
Karim Kawambwa <karimkawambwa@icloud.com>
2018-08-24 18:19:27 -0400

    psuedo algorithm

--- functions/board.js
+++ functions/board.js
@@ -2,10 +2,24 @@
 // Could use node persist or another key value store to allow multiple unique games
 let currentBoard = [ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ];
 
+// The server will always be 'o'
 const x = 'x', o = 'o';
 
 // Use minimax algorithm to find the best move
-function minimax() {
+function minimax(board, player) {
+  // Determine if a player has already won
+  // Return a score
+  // 0 if tie
+  // -10 if x wins
+  // 10 if o wins
+
+
+  // Find the playable indexes
+
+  // Loop through all the playable indexes
+  // Play each possibility recursively
+  
+  // Return the best move
   return 0;
 }
 
