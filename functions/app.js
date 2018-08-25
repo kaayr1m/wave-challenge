b3637986a3da6688fd333c1ab9bdb190d049734c
Karim Kawambwa <karimkawambwa@icloud.com>
2018-08-24 20:01:08 -0400

    cocurrency

--- functions/app.js
+++ functions/app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const board = require('./board');
 
+// Control access and modifications to the board
+const AsyncLock = require('async-lock');
+const lock = new AsyncLock();
+
 // Print response with '+' as symbol for space
 function toString(board) {
   return board.map(s => s === ' ' ? '+': s).join('');
@@ -10,7 +14,11 @@ function toString(board) {
 
 // Return the current board
 app.get('/', (req, res) => {
-  res.send(toString(board.current()));
+  lock.acquire("board", () => {
+    res.send(toString(board.current()));
+  }).catch(() => {
+      res.sendStatus(400);
+  });
 });
 
 // Play the next move if its bots turn
@@ -25,14 +33,18 @@ app.put('/:board', (req, res) => {
   // Replace '+' to spaces
   newBoard = newBoard.map(s => s === "+" ? " ": s);
 
-  if (board.myTurn()) {
-    let currentBoard = board.play(newBoard);
-    res.send(toString(currentBoard));
-  } else {
-    // Not the servers turn
-    // Bad request
-    res.sendStatus(400);
-  }
+  lock.acquire("board", () => {
+    if (board.myTurn()) {
+      let currentBoard = board.play(newBoard);
+      res.send(toString(currentBoard));
+    } else {
+      // Not the servers turn
+      // Bad request
+      res.sendStatus(400);
+    }
+  }).catch(() => {
+      res.sendStatus(400);
+  });
 });
 
 module.exports = app;
