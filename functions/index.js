dc72564303e21f6a7d6810eaffc7828755e0e241
Karim Kawambwa <karimkawambwa@icloud.com>
2018-08-24 20:50:48 -0400

    fix /board

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,8 @@ const functions = require('firebase-functions');
 const app = require('./app');
 
 exports.board = functions.https.onRequest((request, response) => {
+  if (!request.path) {
+    request.url += "/";
+  }
   app(request, response);
 });
